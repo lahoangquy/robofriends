4bb23225bf4baf48084e56b69a576c5d95af1f6f
lahoangquy <62043234+lahoangquy@users.noreply.github.com>
2020-05-01 17:40:41 +0930

    Add files via upload

---
+++ build/precache-manifest.9a6abae074d05139649e439242a9672f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a10e9d7c8813a6df6d2fe5c2e8b14885",
+    "url": "/robot/index.html"
+  },
+  {
+    "revision": "86e0d156ddf50d32fa4d",
+    "url": "/robot/static/css/2.66b7f74f.chunk.css"
+  },
+  {
+    "revision": "af561b1f2bf5686a5e28",
+    "url": "/robot/static/css/main.01752c4e.chunk.css"
+  },
+  {
+    "revision": "86e0d156ddf50d32fa4d",
+    "url": "/robot/static/js/2.df6ae4d8.chunk.js"
+  },
+  {
+    "revision": "596e4823497e45004d2db4c50beba3f3",
+    "url": "/robot/static/js/2.df6ae4d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af561b1f2bf5686a5e28",
+    "url": "/robot/static/js/main.8c1e4fb3.chunk.js"
+  },
+  {
+    "revision": "b8c6c8734e43ce3e41e5",
+    "url": "/robot/static/js/runtime-main.32e3b786.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robot/static/media/SEGA.12a3db8b.woff"
+  }
+]);
